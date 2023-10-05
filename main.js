c0d6ca223391ad6ee2ea15796101b4187b54355b
tphill <tphill760@gmail.com>
2023-10-04 21:05:41 -0400

    i got divs!

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createBoard(){
     startCells.forEach((cell, index) =>{
         const cellElement = document.createElement('div') // creating div elements
         cellElement.classList.add('square') // giving those divs a class of square
+        cellElement.id = index
         gameBoard.append(cellElement) // appending cell to div
     })
 }
