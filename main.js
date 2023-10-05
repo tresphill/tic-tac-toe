d71099c7facd84d9b44dc758c290d70570d9914f
tphill <tphill760@gmail.com>
2023-10-04 20:16:27 -0400

    adding css

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+// setting variables
+
 const gameBoard = document.querySelector('#gameboard')
 const infoDisplay = document.querySelector('#info') 
 const startCells = [
     "", "", "", "", "", "", "", "", "",
 ]
 
+// function for rendering board
+
 function createBoard(){
     startCells.forEach((cell, index) =>{
-        const cellElement = document.createElement('div')
-        cellElement.classList.add('square')
+        const cellElement = document.createElement('div') // creating div elements
+        cellElement.classList.add('square') // giving those divs a class of square
+        gameBoard.append(cellElement) // appending cell to div
     })
 }
+createBoard()
