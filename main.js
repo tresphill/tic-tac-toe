a99900c853bdb9addd932bee613914f1f8fe136a
tphill <tphill760@gmail.com>
2023-10-04 21:19:13 -0400

    circles

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ function createBoard(){
     startCells.forEach((cell, index) =>{
         const cellElement = document.createElement('div') // creating div elements
         cellElement.classList.add('square') // giving those divs a class of square
-        cellElement.id = index
+        cellElement.id = index // assigning id to divs
+        cellElement.addEventListener('click', addTurn) // adding event listener to each click ona  cell, running function addTurn
         gameBoard.append(cellElement) // appending cell to div
     })
 }
 createBoard()
+
+// function for adding moves to gameboard
+
+function addTurn(e){
+    const goTurn = document.createElement('div') // function for creating circle div
+    goTurn.classList.add('circle') // adding class of 'circle'
+    e.target.append(goTurn) // targeting thr event listener 
+}
